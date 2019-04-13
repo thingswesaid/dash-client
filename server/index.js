1c67793388eecfe3a3fd6074ef3aba8accbf96d3
Manuel Di Cristo <manuel.dicristo@icloud.com>
2019-04-13 13:08:25 -0700

    [server] catch all route attempt

--- server/index.js
+++ server/index.js
@@ -22,4 +22,12 @@ const server = http.createServer(app);
 
 app.use(express.static(path.join(__dirname, '../build')));
 
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../build/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+
 server.listen(port, () => console.log(`Running on localhost:${port}`));
