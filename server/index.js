19a6013cd865e870fefee97c804ef83d6cfed3dd
Manuel Di Cristo <manuel.dicristo@icloud.com>
2019-04-13 11:58:59 -0700

    [server] catch all route

--- server/index.js
+++ server/index.js
@@ -12,7 +12,6 @@
 
 // server.listen(port, () => console.log(`Running on localhost:${port}`));
 
-
 const express = require('express');
 const http = require('http');
 const path = require('path');
@@ -22,8 +21,5 @@ const port = process.env.PORT || '8080';
 const server = http.createServer(app);
 
 app.use(express.static(path.join(__dirname, '../build')));
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'../build/index.html'));
-});
 
 server.listen(port, () => console.log(`Running on localhost:${port}`));
