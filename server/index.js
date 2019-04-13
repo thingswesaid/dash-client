c353d08677ea6143acd37e53c5f4e8d4879de695
Manuel Di Cristo <manuel.dicristo@icloud.com>
2019-04-13 10:38:25 -0700

    [server] routing issue fix attempt

--- server/index.js
+++ server/index.js
@@ -1,13 +1,31 @@
+// const express = require('express');
+// const http = require('http');
+// const path = require('path');
+
+// let app = express();
+// const port = process.env.PORT || '8080';
+// const server = http.createServer(app);
+
+// app.use(express.static(path.join(__dirname, '../build')));
+
+// app.set('port', port);
+
+// server.listen(port, () => console.log(`Running on localhost:${port}`));
+
+
 const express = require('express');
 const http = require('http');
 const path = require('path');
 
-let app = express();
+const app = express();
 const port = process.env.PORT || '8080';
 const server = http.createServer(app);
 
 app.use(express.static(path.join(__dirname, '../build')));
 
-app.set('port', port);
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'../build/index.html'));
+});
 
 server.listen(port, () => console.log(`Running on localhost:${port}`));
