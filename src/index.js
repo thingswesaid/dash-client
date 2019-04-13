583cf41a7a50a542f686a061bade4a3eccff5c74
Manuel Di Cristo <manuel.dicristo@icloud.com>
2019-04-13 14:27:49 -0700

    [+] routing + components fixes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo'
 import ApolloClient from 'apollo-boost'
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
+import AppFrame from './components/app-frame'
+import FeedPage from './pages/FeedPage'
+import DraftsPage from './pages/DraftsPage'
+import CreatePage from './pages/CreatePage'
+import DetailPage from './pages/DetailPage'
+import VideoPage from './pages/video'
 
 import './index.css';
-import App from './App';
 
 const client = new ApolloClient({ uri: 'http://localhost:4000' })
 
 render((
   <ApolloProvider client={client}>
     <Router>
-      <App/>
+      <AppFrame>
+        <Switch>
+          <Route exact path="/" component={FeedPage} />
+          <Route path="/drafts" component={DraftsPage} />
+          <Route path="/create" component={CreatePage} />
+          <Route path="/post/:id" component={DetailPage} />
+          <Route path="/video/:id" component={VideoPage} />
+        </Switch>
+      </AppFrame>
     </Router>
   </ApolloProvider>
 ), document.getElementById('root'));
