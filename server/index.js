d06ce033f409c12afd32abcae8d4d30d02d8fca6
Manuel Di Cristo <manuel.dicristo@icloud.com>
2019-04-13 12:48:43 -0700

    [engines] remove

--- server/index.js
+++ server/index.js
@@ -3,11 +3,11 @@
 // const path = require('path');
 
 // let app = express();
+
 // const port = process.env.PORT || '8080';
 // const server = http.createServer(app);
 
 // app.use(express.static(path.join(__dirname, '../build')));
-
 // app.set('port', port);
 
 // server.listen(port, () => console.log(`Running on localhost:${port}`));
